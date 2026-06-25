372ffb35f80a5d6fe39f72af3665efcdedd6c2f0
rnegrao <renatonegraooliveira@gmail.com>
2026-06-25 17:51:34 -0300

    Add boudary condition parameters and update source settings
    Add boudary condition parameters and update source settings

--- 1D_FDTD.py
+++ 1D_FDTD.py
@@ -6,16 +6,22 @@ def epsilon_r(z_val):
     epsilon_vacuum = np.empty_like(z_val)
     epsilon_r = np.empty_like(z_val)
     epsilon_vacuum_mask = np.abs(z_val) >= 2.0
-    epsilon_vacuum[epsilon_vacuum_mask] = 1. 
+    epsilon_vacuum[epsilon_vacuum_mask] = 1.0 
     epsilon_r_mask = np.abs(z_val) < 2.0
-    epsilon_r[epsilon_r_mask] = 10
+    epsilon_r[epsilon_r_mask] = 6.0
     epsilon_i = epsilon_r + epsilon_vacuum
 
     return epsilon_i
 
 def mu_r(z_val):
-
-    return 1.
+    mu_vacuum = np.empty_like(z_val)
+    mu_r = np.empty_like(z_val)
+    mu_vacuum_mask = np.abs(z_val) >= 2.0
+    mu_vacuum[mu_vacuum_mask] = 1.0 
+    mu_r_mask = np.abs(z_val) < 2.0
+    mu_r[mu_r_mask] = 2.0
+    mu_i = mu_r + mu_vacuum
+    return mu_i
 
 if __name__ == "__main__":
     '''compute grid resolution'''
@@ -23,7 +29,7 @@ if __name__ == "__main__":
     dz = h
     '''compute timestep - Courant stability condition'''
     dt = h/( 2. * constant.speed_of_light )
-    t_val = np.arange(0. , 20.e-14, dt)
+    t_val = np.arange(0. , 15.e-14, dt)
     '''1D medium geometry definition'''
     L = 5e-6
     z_val = np.arange(-L, L, dz)
@@ -33,26 +39,30 @@ if __name__ == "__main__":
     me = constant.speed_of_light * dt / ( epsilon_r * dz )
     mh = constant.speed_of_light * dt / ( mu_r * dz )
     '''source settings (gaussian pulse) 750 THz (UV)  '''
-    source_pos = z_val.size // 2
+    source_pos = z_val.size // 8
     f = 750e12
     t0 = 6 * 0.5 / f       
     spread = 0.5 / f
+    A = - np.sqrt( epsilon_r[source_pos] / mu_r[source_pos] )
     '''initialize field components'''
     Ez = np.zeros(z_val.size)
     Hy = np.zeros(z_val.size)
     Ez_source = np.zeros(z_val.size)
-
+    
     plt.figure(figsize=(10,4))        
     for n in range(t_val.size):     
         '''update Magnetic Field Hy using spatial derivatives of Ez'''
         for i in range(z_val.size-1):
-            Hy[i] = Hy[i] - mh * ( Ez[i+1] - Ez[i] )
+            Hy[i] = Hy[i] - mh[i] * ( Ez[i+1] - Ez[i] )
+        Hy[1000] = Hy[1000] - mh[1000] * ( 0.0 - Ez[1000] )  
+        
         '''update Electric Field Ez using spatial derivatives of Hy'''
+        Ez[0] = Ez[0] - me[0] * ( Hy[0] - 0.0 )
         for i in range(1, z_val.size):
-            Ez[i] = Ez[i] - me[i] * ( Hy[i] - Hy[i-1] )        
+            Ez[i] = Ez[i] - me[i] * ( Hy[i] - Hy[i-1] )
             
         '''inject Source Wave (source at the center)''' 
-        pulse = np.exp( - (( t_val[n] - t0)/spread ) ** 2 )
+        pulse = A * np.exp( - (( t_val[n] - t0)/spread ) ** 2 )
         Ez_source[source_pos] = pulse
         Ez = Ez + Ez_source
 
@@ -66,4 +76,4 @@ if __name__ == "__main__":
         plt.grid(True)
         plt.legend()
         plt.draw()
-        plt.pause(1e-6)
+        plt.pause(1e-7)
