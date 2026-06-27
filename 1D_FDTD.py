50ca7fa1b41f6e9131adf7b6ec10919a25add2d9
rnegrao <renatonegraooliveira@gmail.com>
2026-06-27 16:21:42 -0300

    Refine field update mechanism
    Adjusted source position and modified field update logic for better accuracy.

--- 1D_FDTD.py
+++ 1D_FDTD.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     me = constant.speed_of_light * dt / ( epsilon_r * dz )
     mh = constant.speed_of_light * dt / ( mu_r * dz )
     '''source settings (gaussian pulse) 750 THz (UV)  '''
-    source_pos = z_val.size // 8
+    source_pos = z_val.size // 10
     f = 750e12
     t0 = 6 * 0.5 / f       
     spread = 0.5 / f
@@ -48,24 +48,68 @@ if __name__ == "__main__":
     Ez = np.zeros(z_val.size)
     Hy = np.zeros(z_val.size)
     Ez_source = np.zeros(z_val.size)
+
+    h1 = Hy[0]
+    h2 = h1
+    h3 = h2
+    h4 = h3
+    h5 = h4
+    h6 = h5
+    h7 = h6
+    h8 = h7
+    h9 = h8
+    h10 = h9
     
-    plt.figure(figsize=(10,4))        
+    e1 = Ez[z_val.size-1]
+    e2 = e1
+    e3 = e2
+    e4 = e3
+    e5 = e4
+    e6 = e5
+    e7 = e6
+    e8 = e7
+    e9 = e8
+    e10 = e9
+    
+    
+    plt.figure(figsize=(10,4))
+
     for n in range(t_val.size):     
         '''update Magnetic Field Hy using spatial derivatives of Ez'''
         for i in range(z_val.size-1):
             Hy[i] = Hy[i] - mh[i] * ( Ez[i+1] - Ez[i] )
-        Hy[1000] = Hy[1000] - mh[1000] * ( 0.0 - Ez[1000] )  
+            e10 = e9
+            e9 = e8
+            e8 = e7
+            e7 = e6
+            e6 = e5
+            e5 = e4
+            e4 = e3
+            e3 = e2
+            e2 = e1
+            e1 = Ez[z_val.size-1]
+            Hy[z_val.size-1] = Hy[z_val.size-1] - mh[z_val.size-1] * ( e10 - Ez[z_val.size-1] )
         
         '''update Electric Field Ez using spatial derivatives of Hy'''
-        Ez[0] = Ez[0] - me[0] * ( Hy[0] - 0.0 )
-        for i in range(1, z_val.size):
+        for i in range(z_val.size-1):
             Ez[i] = Ez[i] - me[i] * ( Hy[i] - Hy[i-1] )
-            
+            h10 = h9
+            h9 = h8
+            h8 = h7
+            h7 = h6
+            h6 = h5
+            h5 = h4
+            h4 = h3
+            h3 = h2
+            h2 = h1
+            h1 = Hy[0]
+            Ez[0] = Ez[0] - me[0] * ( Hy[0] - h5 )    
+         
         '''inject Source Wave (source at the center)''' 
         pulse = A * np.exp( - (( t_val[n] - t0)/spread ) ** 2 )
         Ez_source[source_pos] = pulse
         Ez = Ez + Ez_source
-
+    
         '''plot final timestep'''
         plt.clf()
         plt.title(f"1D FDTD Ex/Hy Wave Propagation (Gaussian Pulse). Time: {t_val[n]} (s)")
